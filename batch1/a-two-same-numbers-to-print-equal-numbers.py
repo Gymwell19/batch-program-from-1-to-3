3964e3cc81326cd6ba375a6eee87a73ff504bb0d
Gymwell19 <ajimuelong@gmail.com>
2025-03-14 00:15:01 +0800

    batch 1.1

--- batch1/a-two-same-numbers-to-print-equal-numbers.py
+++ batch1/a-two-same-numbers-to-print-equal-numbers.py
@@ -1,4 +1,7 @@
 # two equal numbers
-a, b= map(int, input("enter two numbers:").split())
-if (a==b):
-    print("equal numbers")
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 == num2:
+    print("Equal numbers")
